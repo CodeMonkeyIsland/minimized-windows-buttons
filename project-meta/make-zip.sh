a384d4da1ea8ba099f678c5928ceca0c43671a03
dani <bla@bla.org>
2025-10-21 13:38:55 +0200

    letting gnome-extensions do the zipping

---
+++ project-meta/make-zip.sh
@@ -0,0 +1,2 @@
+#zip -r minimized-windows-buttons@code-monkey-island.ch.zip minimized-windows-buttons@code-monkey-island.ch
+gnome-extensions pack minimized-windows-buttons@code-monkey-island.ch --force
